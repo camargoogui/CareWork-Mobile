a8962926b4ffc165985966f6c36678726871fa52
˜camargoogui˜ <guipcamargo64@gmail.com>
2025-11-17 17:34:36 -0300

    chore: configuração de lint e formatação
    - ESLint config
    - Prettier config

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react', 'react-hooks', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+  },
+  env: {
+    node: true,
+    es6: true,
+    'react-native/react-native': true,
+  },
+};
+
