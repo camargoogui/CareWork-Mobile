44b7bd414e4b124874628cde72e16d2721c6d4c6
˜camargoogui˜ <guipcamargo64@gmail.com>
2025-11-17 17:34:37 -0300

    chore: exports centralizados das telas

---
+++ src/screens/index.ts
@@ -0,0 +1,15 @@
+export { LoginScreen } from './LoginScreen';
+export { SignupScreen } from './SignupScreen';
+export { HomeScreen } from './HomeScreen';
+export { CheckinScreen } from './CheckinScreen';
+export { CheckinHistoryScreen } from './CheckinHistoryScreen';
+export { EditCheckinModal } from './EditCheckinModal';
+export { TipsScreen } from './TipsScreen';
+export { ReportScreen } from './ReportScreen';
+export { ProfileScreen } from './ProfileScreen';
+export { AboutScreen } from './AboutScreen';
+export { HelpScreen } from './HelpScreen';
+export { EditProfileScreen } from './EditProfileScreen';
+export { ChangePasswordScreen } from './ChangePasswordScreen';
+export { DeleteAccountScreen } from './DeleteAccountScreen';
+
